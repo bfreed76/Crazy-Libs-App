0451818e355ccfcafd2381e33f80edf9de40fd07
Brinton <brintonfoyreed@gmail.com>
2021-06-23 12:34:59 -0700

    Refactored file structure

---
+++ src/Containers/FetchContainer.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import StoryContainer from './StoryContainer'
+
+const FetchContainer = () => {
+    const [story, setStory] = useState([])
+    const [hasError, setHasError] = useState(false)
+
+    useEffect(() => newStory(), [])
+
+    const newStory = () => {
+        fetch("http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=25") 
+        .then(res => res.json())
+        .then(res => setStory(res))
+        .catch(err => setHasError(true), [])
+    }
+
+    return (
+        <div>
+            <StoryContainer storyText={story} newStory={newStory}/>
+        </div>
+    )
+}
+
+{/*powered by MadlibzAPI: https://madlibz.herokuapp.com/api#api*/}
+
+export default FetchContainer;
--- src/Containers/StoryContainer.js
+++ src/Containers/StoryContainer.js
@@ -1,26 +1,60 @@
 import React, { useState, useEffect } from 'react'
+import Inputs from '../Components/Inputs'
 import Story from '../Components/Story'
 
-const StoryContainer = () => {
-    const [story, setStory] = useState([])
-    const [hasError, setHasError] = useState(false)
+const StoryContainer = (props) => {
+    const {title, blanks, value} = props.storyText
+    const [finished, setFinished] = useState(false);
+    const [input, setInput] = useState({});
 
-    useEffect(() => newStory(), [])
 
-    const newStory = () => {
-        fetch("http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=25") 
-        .then(res => res.json())
-        .then(res => setStory(res))
-        .catch(err => setHasError(true), [])
+    const inputToState = (e) => {
+        e.preventDefault()
+        const {name, value} = e.target
+        setInput(prevState => {return {...prevState, [name]: value}})  
     }
+
+    const displayBlanks = () => {
+        if(blanks) {
+            return blanks.map((blank, id) => {
+                return <Inputs blank={blank} key={id} inputToState={inputToState} id={id}/>
+                }
+            )
+        } 
+    }
+
+    const renderStory = () => {
+        setFinished(true)
+        let counter = 0
+         let mapping = value.map((line) => {
+             return line + input[counter]
+             counter += 1
+         })
+        // let finishedStory = mapping.join(" ")
+        console.log("Render Story Finished")
+        return <Story finishedStory={mapping} />
+        
+    }
+
+    const newStoryClick = () => {
+        setFinished(false)
+        return props.newStory()
+    }
+
 
     return (
         <div>
-            <Story storyText={story} newStory={newStory}/>
+            <h1>Loco Libs</h1>
+            <p>Fill in the blanks then click the finished button below to share your story with others!</p>
+            <hr></hr>
+            <button onClick={newStoryClick}> NEW STORY </button>
+            <h3>{title}</h3>
+            {!finished ? displayBlanks() : console.log("finished")}
+            <br></br>
+            <button onClick={renderStory}>FINISHED</button>
+            <br></br>
         </div>
     )
 }
 
-{/*powered by MadlibzAPI: https://madlibz.herokuapp.com/api#api*/}
-
-export default StoryContainer;
+export default StoryContainer
